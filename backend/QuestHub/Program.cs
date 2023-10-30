1b00b049ae0d883f34b61f9efba2bc60c39595e2
Sameera <srasanga999@gmail.com>
2023-10-30 19:29:12 +0000

    Db migratoins using DB up

--- backend/QuestHub/Program.cs
+++ backend/QuestHub/Program.cs
@@ -1,9 +1,30 @@
+using DbUp;
+using System.Net.WebSockets;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//create a databae if it doesnt exisits
+EnsureDatabase.For.SqlDatabase(connectionString);
+
+//look for sql scripts that embeded in the project
+//and do db migrations in a transaction
+var upgrader = DeployChanges.To
+    .SqlDatabase(connectionString, null)
+    .WithScriptsEmbeddedInAssembly(System.Reflection.Assembly.GetExecutingAssembly())
+    .WithTransaction()
+    .Build();
+// upgrade DB if there are pending queries
+    if (upgrader.IsUpgradeRequired())
+    {
+        upgrader.PerformUpgrade();
+    }
+
 
 var app = builder.Build();
 
