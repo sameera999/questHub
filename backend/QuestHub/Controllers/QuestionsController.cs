69a097f9f400fd36ef6db4e87159da9feaee5d29
Sameera <srasanga999@gmail.com>
2023-10-30 22:31:21 +0000

    Add post and put methods in questions

--- backend/QuestHub/Controllers/QuestionsController.cs
+++ backend/QuestHub/Controllers/QuestionsController.cs
@@ -19,16 +19,62 @@ namespace QuestHub.Controllers
 
        
         [HttpGet]
-        public IEnumerable<QuestionGetManyResponse> GetQuestions(string search)
+        public IEnumerable<QuestionGetManyResponse> GetQuestions(string? search)
         {
+            //extend search questions
             if (string.IsNullOrEmpty(search))
             {
-                return _dataRepository.GetQuestionsBySearch(search);
+                return _dataRepository.GetQuestions();
             }
             else
             {
                 return _dataRepository.GetQuestionsBySearch(search);
             }
+        }
+
+        [HttpGet("unanswered")]
+        public IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions()
+        {
+            return _dataRepository.GetUnansweredQuestions();
+        }
+
+        [HttpGet("{questionId}")]
+        public ActionResult<QuestionGetSingleResponse> GetQuestion(int questionId)
+        {
+            var question = _dataRepository.GetQuestion(questionId);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return question;
+        }
+
+        [HttpPost]
+        public ActionResult<QuestionGetSingleResponse> PostQuestion(QuestionPostRequest question)
+        {
+            var savedQuestion = _dataRepository.PostQuestion(question);
+            return CreatedAtAction(nameof(GetQuestion)
+                , new {questionId = savedQuestion.QuestionId}
+                , savedQuestion);
+        }
+
+        [HttpPut("{questionId}")]
+        public ActionResult<QuestionGetSingleResponse> PutQuestion(int questionId, QuestionPutRequest questionPutRequest)
+        {
+            var question = _dataRepository.GetQuestion(questionId);
+            if (question.QuestionId == null)
+            {
+                return NotFound();
+            }
+
+            questionPutRequest.Title = string.IsNullOrEmpty(questionPutRequest.Title) 
+                ? question.Title : questionPutRequest.Title;
+            questionPutRequest.Content = string.IsNullOrEmpty(questionPutRequest.Content) 
+                ? question.Content : questionPutRequest.Content;
+
+            var savedQuestion = _dataRepository.PutQuestion(questionId, questionPutRequest);
+            return savedQuestion;
         }
     }
 }
