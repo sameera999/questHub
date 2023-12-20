7073d8f0d7f3aba454dfd12be702bce862ca0d55
Sameera <srasanga999@gmail.com>
2023-12-20 20:56:03 +0000

    lazy loading the ask page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,10 +7,10 @@ import { css } from '@emotion/react';
 import { fontFamily, fontSize, gray2 } from './Styles';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { SearchPage } from './SearchPage';
-import { AskPage } from './AskPage';
 import { SignInPage } from './SignInPage';
 import { NotFoundPage } from './NotFoundPage';
 import { QuestionPage } from './QuestionPage';
+const AskPage = React.lazy(() => import('./AskPage'));
 
 function App() {
   const unused = 'something';
@@ -28,7 +28,25 @@ function App() {
         <Routes>
           <Route path="" element={<HomePage />} />
           <Route path="search" element={<SearchPage />} />
-          <Route path="ask" element={<AskPage />} />
+          <Route
+            path="ask"
+            element={
+              <React.Suspense
+                fallback={
+                  <div
+                    css={css`
+                      margin-top: 100px;
+                      text-align: center;
+                    `}
+                  >
+                    Loading...
+                  </div>
+                }
+              >
+                <AskPage />
+              </React.Suspense>
+            }
+          />
           <Route path="signin" element={<SignInPage />} />
           <Route path="questions/:questionId" element={<QuestionPage />} />
           <Route path="*" element={<NotFoundPage />} />
