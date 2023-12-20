4142c268733c38c541b923116c0c90484caf6b40
Sameera <srasanga999@gmail.com>
2023-12-20 22:09:31 +0000

    implemented question form and installed react hook form

--- frontend/src/AskPage.tsx
+++ frontend/src/AskPage.tsx
@@ -1,5 +1,42 @@
 import React from 'react';
 import { Page } from './Page';
-export const AskPage = () => <Page title="Ask a question">{null}</Page>;
+import { useForm } from 'react-hook-form';
+import {
+  FieldContainer,
+  FieldInput,
+  FieldLabel,
+  FieldTextArea,
+  Fieldset,
+  FormButtonContainer,
+  PrimaryButton,
+} from './Styles';
+
+type FormData = {
+  title: string;
+  content: string;
+};
+
+export const AskPage = () => {
+  const { register } = useForm<FormData>();
+  return (
+    <Page title="Ask a question">
+      <form>
+        <Fieldset>
+          <FieldContainer>
+            <FieldLabel htmlFor="title">Title</FieldLabel>
+            <FieldInput id="title" type="text" {...register('title')} />
+          </FieldContainer>
+          <FieldContainer>
+            <FieldLabel htmlFor="content">Content</FieldLabel>
+            <FieldTextArea id="content" {...register('content')} />
+          </FieldContainer>
+          <FormButtonContainer>
+            <PrimaryButton type="submit">Submit Your Question</PrimaryButton>
+          </FormButtonContainer>
+        </Fieldset>
+      </form>
+    </Page>
+  );
+};
 
 export default AskPage;
--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -3,12 +3,26 @@ import { css } from '@emotion/react';
 import { fontFamily, fontSize, gray1, gray2, gray5 } from './Styles';
 import React from 'react';
 import { UserIcon } from './Icons';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+  search: string;
+};
 
 export const Header = () => {
+  const [searchParams] = useSearchParams();
+  const criteria = searchParams.get('criteria') || '';
+  const { register } = useForm<FormData>();
+
   const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.currentTarget.value);
+    // setSearch(e.currentTarget.value);
   };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <div
       css={css`
@@ -36,26 +50,31 @@ export const Header = () => {
       >
         QuestHub
       </Link>
-      <input
-        type="text"
-        placeholder="Search..."
-        onChange={handleSearchInputChange}
-        css={css`
-          box-sizing: border-box;
-          font-family: ${fontFamily};
-          font-size: ${fontSize};
-          padding: 8px 10px;
-          border: 1px solid ${gray5};
-          border-radius: 3px;
-          color: ${gray2};
-          background-color: white;
-          width: 200px;
-          height: 30px;
-          :focus {
-            outline-color: ${gray5};
-          }
-        `}
-      />
+
+      <form onSubmit={handleSubmit}>
+        <input
+          {...register('search')}
+          type="text"
+          placeholder="Search..."
+          defaultValue={criteria}
+          css={css`
+            box-sizing: border-box;
+            font-family: ${fontFamily};
+            font-size: ${fontSize};
+            padding: 8px 10px;
+            border: 1px solid ${gray5};
+            border-radius: 3px;
+            color: ${gray2};
+            background-color: white;
+            width: 200px;
+            height: 30px;
+            :focus {
+              outline-color: ${gray5};
+            }
+          `}
+        />
+      </form>
+
       <Link
         to="signin"
         css={css`
