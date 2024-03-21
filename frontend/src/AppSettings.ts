2f71867346ba411e946b5847f52cfdb5e0a39a1a
Sameera <srasanga999@gmail.com>
2024-03-21 19:07:23 +0000

    Configured Auth0 settings in our frontend

--- frontend/src/AppSettings.ts
+++ frontend/src/AppSettings.ts
@@ -3,8 +3,8 @@ export const server = 'https://localhost:7160';
 export const webAPIUrl = `${server}/api`;
 
 export const authSettings = {
-  domain: 'your-tenantid.auth0.com',
-  client_id: 'your-clientid',
+  domain: 'dev-qqff7iofsebhfvpq.us.auth0.com',
+  client_id: '68X0UCzHdTyXSskKjTN1CX9tkR7gMcNA',
   redirect_uri: window.location.origin + '/signin-callback',
   scope: 'openid profile QandAAPI email',
   audience: 'https://questhub',
